b6a06ea33a5d255980dbc85ee2296d4a2cff8444
MatheusBenetti <mbbenetti1@gmail.com>
2022-12-20 11:05:12 -0300

    changed the app const on file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const app = require('./app');
 
 dotenv.config();
+
+const app = require('./app');
 
 // const database = process.env.DATABASE_CONNECTION;
 
