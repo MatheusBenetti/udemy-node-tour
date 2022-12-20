e12f5683a90349bdb3e563d043a6fd0b1883a3ac
MatheusBenetti <mbbenetti1@gmail.com>
2022-12-20 11:03:46 -0300

    update model

--- src/models/tourModel.js
+++ src/models/tourModel.js
@@ -5,15 +5,42 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name.'],
         unique: true,
+        trim: true,
     },
-    rating: {
+    ratingAverage: {
         type: Number,
         default: 3.0,
+    },
+    ratingQuantity: {
+        type: Number,
+        default: 0,
     },
     price: {
         type: Number,
         required: [true, 'A tour must have a price.'],
     },
+    discountPrice: Number,
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration.'],
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'The tour need to have a maximum number of people'],
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'People must know if they are able go to the tour'],
+    },
+    summery: {
+        type: String,
+        trim: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    startDates: [Date],
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
