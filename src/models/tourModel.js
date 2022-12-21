04e066c5e34e2c564446d743333f1206bc61eaf3
MatheusBenetti <mbbenetti1@gmail.com>
2022-12-21 11:37:05 -0300

    created virtual properties on model

--- src/models/tourModel.js
+++ src/models/tourModel.js
@@ -1,47 +1,64 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name.'],
-        unique: true,
-        trim: true,
+const tourSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'A tour must have a name.'],
+            unique: true,
+            trim: true,
+        },
+        ratingAverage: {
+            type: Number,
+            default: 3.0,
+        },
+        ratingQuantity: {
+            type: Number,
+            default: 0,
+        },
+        price: {
+            type: Number,
+            required: [true, 'A tour must have a price.'],
+        },
+        discountPrice: Number,
+        duration: {
+            type: Number,
+            required: [true, 'A tour must have a duration.'],
+        },
+        maxGroupSize: {
+            type: Number,
+            required: [
+                true,
+                'The tour need to have a maximum number of people',
+            ],
+        },
+        difficulty: {
+            type: String,
+            required: [
+                true,
+                'People must know if they are able go to the tour',
+            ],
+        },
+        summary: {
+            type: String,
+            trim: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        startDates: [Date],
     },
-    ratingAverage: {
-        type: Number,
-        default: 3.0,
-    },
-    ratingQuantity: {
-        type: Number,
-        default: 0,
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price.'],
-    },
-    discountPrice: Number,
-    duration: {
-        type: Number,
-        required: [true, 'A tour must have a duration.'],
-    },
-    maxGroupSize: {
-        type: Number,
-        required: [true, 'The tour need to have a maximum number of people'],
-    },
-    difficulty: {
-        type: String,
-        required: [true, 'People must know if they are able go to the tour'],
-    },
-    summary: {
-        type: String,
-        trim: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-    startDates: [Date],
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7;
 });
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
