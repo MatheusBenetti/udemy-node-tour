b5d6c1ce2ed70a209197a10f3d14789788add9f2
MatheusBenetti <mbbenetti1@gmail.com>
2022-12-22 14:30:09 -0300

    route error handling

--- app.js
+++ app.js
@@ -11,16 +11,18 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log('Middleware!');
-    next();
-});
-
-app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'Fail.',
+        message: `Can't find ${req.originalUrl} on this server.`,
+    });
+});
 
 module.exports = app;
