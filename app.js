e7599d9b1b3d7d87341996107ee72ffc2ecfaf8e
MatheusBenetti <mbbenetti1@gmail.com>
2022-12-26 10:06:29 -0300

    Error middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const globalErrorHandler = require('./src/controllers/errorController');
+const AppError = require('./src/utils/appErrors');
 const tourRouter = require('./src/routes/tourRoutes');
 const userRouter = require('./src/routes/userRoutes');
 
@@ -11,18 +13,17 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'Fail.',
-        message: `Can't find ${req.originalUrl} on this server.`,
-    });
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
