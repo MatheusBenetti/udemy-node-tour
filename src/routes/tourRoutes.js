f1672dff35b38f637784897ee132b1b2d8428752
MatheusBenetti <mbbenetti1@gmail.com>
2022-12-26 12:20:49 -0300

    refactor monthly plan route

--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -6,22 +6,22 @@ const router = express.Router();
 // router.param('id', tourController.checkId);
 
 router
-    .route('/top-five-cheap')
-    .get(tourController.aliasTopTours, tourController.getAllTours);
+  .route('/top-five-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
 
-router.route('/:year').get(tourController.getMonthlyPlan);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
-    .route('/')
-    .get(tourController.getAllTours)
-    .post(tourController.createTour);
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 
 router
-    .route('/:id')
-    .get(tourController.getTourById)
-    .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+  .route('/:id')
+  .get(tourController.getTourById)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
