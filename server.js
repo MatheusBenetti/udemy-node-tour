7ecefbb187e42ea4c471e42652dd78c5ffaa3905
MatheusBenetti <mbbenetti1@gmail.com>
2022-12-26 14:49:13 -0300

    uncaughtException unhandledRejection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config();
 
 const app = require('./app');
@@ -8,14 +13,22 @@ const app = require('./app');
 // const database = process.env.DATABASE_CONNECTION;
 
 mongoose
-    // .connect(database, {
-    //     useNewUrlParser: true,
-    // })
-    // .then(console.log('DB connection successful.'));
-    .connect(process.env.DATABASE_LOCAL)
-    .then(console.log('DB connection successful.'));
+  .set({ strictQuery: false })
+  // .connect(database, {
+  //     useNewUrlParser: true,
+  // })
+  // .then(console.log('DB connection successful.'));
+  .connect(process.env.DATABASE_LOCAL)
+  .then(console.log('DB connection successful.'));
 
 const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}.`);
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
