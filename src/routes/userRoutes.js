5430728b77536203e0cb47da6986b7d6220cc7c3
MatheusBenetti <mbbenetti1@gmail.com>
2022-12-27 09:50:26 -0300

    update userRoutes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router
-    .route('/')
-    .get(userController.getAllUsers)
-    .post(userController.createUsers);
+router.post('/signup', authController.signUp);
 
 router
-    .route('/:id')
-    .get(userController.getUsersById)
-    .patch(userController.updateUsers)
-    .delete(userController.deleteUsers);
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUsers);
+
+router
+  .route('/:id')
+  .get(userController.getUsersById)
+  .patch(userController.updateUsers)
+  .delete(userController.deleteUsers);
 
 module.exports = router;
