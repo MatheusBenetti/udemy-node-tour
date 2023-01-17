d6c4c831310e04c2493e36adbfe5362d843769aa
MatheusBenetti <mbbenetti1@gmail.com>
2023-01-17 12:16:06 -0300

    endpoint to update just the selected user fields password and roles can't be updated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,14 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appErrors');
+const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = async (req, res, next) => {
   const users = await User.find();
@@ -11,6 +21,24 @@ exports.getAllUsers = async (req, res, next) => {
     }
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.confirmPassword) {
+    return next(new AppError(`This route isn't for password updates. `, 400));
+  }
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'Success.',
+    user: { updatedUser }
+  });
+});
 
 exports.getUsersById = (req, res) => {};
 
