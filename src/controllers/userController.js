af0eb706952103f880ab52db17b838e26e79a146
MatheusBenetti <mbbenetti1@gmail.com>
2023-01-18 09:28:29 -0300

    delete current user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -40,6 +40,15 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'Success.',
+    data: null
+  });
+});
+
 exports.getUsersById = (req, res) => {};
 
 exports.createUsers = (req, res) => {};
