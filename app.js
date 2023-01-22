a9d87fc19177809c6cb62af59550aa28b4ae01db
MatheusBenetti <mbbenetti1@gmail.com>
2023-01-22 13:14:39 -0300

    preventing parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const globalErrorHandler = require('./src/controllers/errorController');
 const AppError = require('./src/utils/appErrors');
@@ -26,6 +27,19 @@ app.use('/api', limiter);
 app.use(mongoSanitize());
 
 app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingQuantity',
+      'ratingAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 app.use(express.json());
 
