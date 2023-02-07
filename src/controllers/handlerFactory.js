acb1fe57974ea7ec53e734df3f3b03f9a6c965ac
MatheusBenetti <mbbenetti1@gmail.com>
2023-02-07 14:21:33 -0300

    update handler function

--- src/controllers/handlerFactory.js
+++ src/controllers/handlerFactory.js
@@ -1,6 +1,25 @@
 const AppError = require('../utils/appErrors');
 const catchAsync = require('../utils/catchAsync');
 
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID.', 404));
+    }
+
+    res.status(200).json({
+      status: 'Success!',
+      data: {
+        tour: doc
+      }
+    });
+  });
+
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
