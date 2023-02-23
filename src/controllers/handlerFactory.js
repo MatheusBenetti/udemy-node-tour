5ca7a8e39c7ff9bb86247327394ed3890a39ee16
MatheusBenetti <mbbenetti1@gmail.com>
2023-02-23 14:42:29 -0300

    reading handlers

--- src/controllers/handlerFactory.js
+++ src/controllers/handlerFactory.js
@@ -1,3 +1,4 @@
+const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appErrors');
 const catchAsync = require('../utils/catchAsync');
 
@@ -43,5 +44,45 @@ exports.deleteOne = (Model) =>
     res.status(204).json({
       status: 'Success!',
       data: null
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID.', 404));
+    }
+
+    res.status(200).json({
+      status: 'Success!',
+      data: {
+        tour: doc
+      }
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allow nested GET reviews on tour.
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    res.status(200).json({
+      status: 'Success!',
+      results: doc.length,
+      data: {
+        data: doc
+      }
     });
   });
